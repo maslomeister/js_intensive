1fa3156f3317314df1a3df46a2d9dc627788e3dc
Maslomeister <maslomeister@gmail.com>
2022-08-23 17:41:55 +0300

    fix: return error only when both inputs entered

--- task02/notation.js
+++ task02/notation.js
@@ -1,11 +1,13 @@
-function toNumber(str) {
-  if (typeof str != "string" || isNaN(str)) {
-    throw new TypeError("Некорректный ввод!");
-  }
-  return Number(str);
+function isNumber(str) {
+  typeof str != "string";
+  return !isNaN(str);
 }
 
-const firsValue = toNumber(window.prompt("Введите целое число"));
-const secondValue = toNumber(window.prompt("Введите целое число"));
+const firsValue = window.prompt("Введите целое число");
+const secondValue = window.prompt("Введите целое число");
+
+if (!isNumber(firsValue) || !isNumber(secondValue)) {
+  throw new TypeError("Некорректный ввод!");
+}
 
 console.log(firsValue.toString(secondValue));
